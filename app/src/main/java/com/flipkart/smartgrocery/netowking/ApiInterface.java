0948946a3670d3dda66241384c3153259294ffc6
mohitagarwal <mohit.tibrewal@flipkart.com>
2017-06-22 21:47:13 +0530

    interface

--- app/src/main/java/com/flipkart/smartgrocery/netowking/ApiInterface.java
+++ app/src/main/java/com/flipkart/smartgrocery/netowking/ApiInterface.java
@@ -3,7 +3,7 @@ package com.flipkart.smartgrocery.netowking;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public class ApiInterface {
+public interface ApiInterface {
 
 //    @GET("directions/json?")
 //    Call<MapsDirectionResponse> getDuration(@Query("origin") String origin, @Query("destination") String destination, @Query("departure_time") long departureTime, @Query("traffic_model") String trafficModel, @Query("key") String key );
